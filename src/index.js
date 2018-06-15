6a036bd8bb804d590ff36a61b1e39f1d2d9c4290
Manoj <manoj.delhi24@gmail.com>
2018-06-15 15:32:06 +0530

    very basic example to understand working of react-router-dom and its Route object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends React.Component {
-
   render() {
-    return <div> Hello!!! </div>;
+    return <div> Home Page !!! </div>;
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+class Hello extends React.Component {
+  render() {
+    return <div> Holu ! </div>;
+  }
+}
+
+class Bye extends React.Component {
+  render() {
+    return <div> Tada ! </div>;
+  }
+}
+
+
+ReactDOM.render( <BrowserRouter>
+  <div>
+    <p>Header</p>
+    <Route path="/hello" component={Hello} />
+    <Route path="/bye" component={Bye} />
+  </div>
+</BrowserRouter>, document.querySelector('.container'))
