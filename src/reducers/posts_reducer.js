73d70cc5f38405bf1b0b4ceaaad4608d04438860
Manoj <manoj.delhi24@gmail.com>
2018-07-08 06:06:40 +0530

    posts data converted to key-value intead of array

--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -1,8 +1,10 @@
 import { FETCH_POSTS } from '../actions';
+import _ from 'lodash';
 
 export default function(state = {}, action) {
   if (action.type === FETCH_POSTS) {
-    return action.payload.data
+    // return _.keyBy(action.payload.data, 'id')
+    return _.mapKeys(action.payload.data, 'id')
   }
   return state;
 }
