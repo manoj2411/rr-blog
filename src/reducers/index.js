defccb7fea6a819f501fe174541e6703ce64ccc6
Manoj <manoj.delhi24@gmail.com>
2018-07-09 20:31:10 +0530

    redux form setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './posts_reducer'
 
 export default combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
