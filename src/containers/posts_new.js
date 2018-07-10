2cfac4664a6a7844e4cd002ad06428acb8f1e47a
Manoj <manoj.delhi24@gmail.com>
2018-07-10 09:02:45 +0530

    Generic Field component method created

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -3,16 +3,38 @@ import { Field, reduxForm } from 'redux-form';
 
 
 class PostsNew extends React.Component {
-  renderTitleField(field) {
-    return <input {...field.input} type="text" />
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input {...field.input}
+          type="text"
+          className="form-control"
+        />
+      </div>
+    );
   }
 
   render() {
     return (
       <div>
-        <div>Posts new</div>
+        <h3>Posts new</h3>
         <form>
-          <Field name="title" component={this.renderTitleField}/>
+          <Field
+            name="title"
+            label="Title: "
+            component={this.renderField}
+          />
+          <Field
+            name="categories"
+            label="Categories: "
+            component={this.renderField}
+          />
+          <Field
+            name="content"
+            label="Content: "
+            component={this.renderField}
+          />
         </form>
       </div>
     );
