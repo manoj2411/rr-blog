45cd031990ec11404f0f532b6281006cf5118c58
Manoj <manoj.delhi24@gmail.com>
2018-07-15 09:23:51 +0530

    form validations and submittal with redux-form

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -11,15 +11,24 @@ class PostsNew extends React.Component {
           type="text"
           className="form-control"
         />
+        {field.meta.error}
       </div>
     );
   }
 
+  submitForm(values) {
+    console.log('submitForm - ');
+    console.log(values)
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div>
         <h3>Posts new</h3>
-        <form>
+        {/*<form onSubmit={handleSubmit(this.submitForm.bind(this))}>*/}
+        <form onSubmit={handleSubmit(this.submitForm)}>
           <Field
             name="title"
             label="Title: "
@@ -35,10 +44,27 @@ class PostsNew extends React.Component {
             label="Content: "
             component={this.renderField}
           />
+          <input type="submit" className="btn btn-primary" />
         </form>
       </div>
     );
   }
 }
 
-export default reduxForm({ form: 'PostNewForm'})(PostsNew)
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title  = "Enter a title!"
+  }
+  if(!values.categories) {
+    errors.categories  = "Enter some categories!"
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content!'
+  }
+
+  return errors;
+}
+
+export default reduxForm({ form: 'PostNewForm', validate})(PostsNew)
